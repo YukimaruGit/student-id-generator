476d82e9766f8b518c9dce84017d32e8cfcd3511
Rose <yukimaru0.0contact@gmail.com>
2025-08-30 23:26:09 +0900

    feat: SNS共有機能の実装とOGP表示の修正

--- functions/ogp/v/[version]/[pid].js
+++ functions/ogp/v/[version]/[pid].js
@@ -1,9 +1,9 @@
-// /functions/ogp/v/[version]/[...pid].js
+// /functions/ogp/v/[version]/[pid].js
 export async function onRequest({ params, request }) {
   const { version, pid } = params;
   
   // Cloudinaryのt_ogp_card変換画像URLにリダイレクト
-  const cloudinaryUrl = `https://res.cloudinary.com/di5xqlddy/image/upload/t_ogp_card/as_chronicle/student_card/${pid.join('/')}.jpg`;
+  const cloudinaryUrl = `https://res.cloudinary.com/di5xqlddy/image/upload/t_ogp_card/as_chronicle/student_card/${pid}.jpg`;
   
   return new Response(null, {
     status: 302,
